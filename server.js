ea23ecefb735e8fdd157521a71d6f0b9b7af3e35
jason durant <durantjay1@gmail.com>
2016-06-16 00:13:18 -0700

    Add new ports

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ server.on('clientError',(err,socket) => {
 	socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
 });
 
-server.listen('8000',"13.91.97.104", () => {
+server.listen(8000,"13.91.97.104", () => {
 	console.log('hey we are connected')
 });
