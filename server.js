7728622658215cce3c2266955c226ad69765c045
jason durant <durantjay1@gmail.com>
2016-06-16 03:06:36 -0700

    Add everything

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer( (req,res) => {
+
+	fs.appendFile('view-header-logs.json', req.headers, (err) => {
+		if(err) throw err;
+		console.log('The data has been appended to the view-header-logs.json file');
+	})
+	res.statusCode = 200;
 	res.end('Hey it works');
 	console.log(req.headers);
+
 });
 
 server.on('clientError',(err,socket) => {
